87881281eb3955743e0f52fcf425ca7e9c6bc9f1
Anthony <anthonymiller@outlook.com>
2014-06-20 12:51:00 -0700

    Bump cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version          '0.1.6'
+version          '0.1.7'
 
 depends			 'homebrew'
 depends          'iptables-ng'
